05e2c0a35f9e951dfeda366e0ae40db58576192f
Renee Sarley <reneesarley@gmail.com>
2018-08-17 12:47:37 -0700

    add two hard codeded courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,10 @@ export class AppComponent {
      new Category  ('Office Productivity'),
      new Category ('Lifestyle')
    ];
+   masterCourseList: Course[] = [
+     new Course('Development', 'Complete Python Bootcamp: Go from zero to hero in Python 3',['Jose Portilla'], 4.5, 80220, 194.99, 10.99, 'Learn Python like a Professional! Start from the basics and go all the way to creating your own applications and games!'),
+     new Course('Development', 'The Web Developer Bootcamp',['Colt Steele'], 4.7, 85435, 199.99, 10.99, 'The only course you need to learn web development - HTML, CSS, JS, Node, and More!')
+   ]
 
 
 }
