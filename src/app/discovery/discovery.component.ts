9e28d0903b306599079f7af1d15d22cfbe876a52
Renee Sarley <reneesarley@gmail.com>
2018-08-17 13:15:28 -0700

    add discovery component

---
+++ src/app/discovery/discovery.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-discovery',
+  templateUrl: './discovery.component.html',
+  styleUrls: ['./discovery.component.css']
+})
+export class DiscoveryComponent {
+  @Input() childCourseList: Course[];
+}
