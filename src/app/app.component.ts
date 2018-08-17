461db334dc653bf47b525d56f064365c44f902a0
Renee Sarley <reneesarley@gmail.com>
2018-08-17 09:43:11 -0700

    add course model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from './models/course.model';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/models/course.model.ts
@@ -0,0 +1,3 @@
+export class Course {
+  constructor( category: string, name: string, instructors: string[], starRating: number, reviewCount: number, originalCost: number, currentCost: number, description: number){};
+}
